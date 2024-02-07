abf32be1acf56e7058ee36c7e362065c728c8afc
Hicham <hichamimad39@gmail.com>
2024-02-07 01:11:45 -0800

    ok second

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
+import { Inter } from 'next/font/google'
 import { ContextProvider } from './context/context'
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
